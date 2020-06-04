e16bfc6f89f502e85f1161900652e7e69931a679
Trung Trinh <you@example.com>
2020-06-03 22:02:58 -0400

    Account done

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -1,5 +1,19 @@
 import React, { Component } from "react";
+import waveImg from "../../assets/wave.png";
+import background from "../../assets/bg.svg";
+import classes from "./Account.module.css";
 
-class Account extends Component {}
+class Account extends Component {
+  render() {
+    return (
+      <div className={classes.container}>
+        <img className={classes.wave} src={waveImg} alt='image'></img>
+        <div className={classes.img}>
+          <img src={background} alt='img'></img>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Account;
